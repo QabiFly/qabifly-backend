6e576df1c335601de03038d8eb086862227a412c
QabiFly <qabifly@zohomail.in>
2026-06-01 19:34:22 +0530

    Add files via upload

---
+++ firebase-credentials.js
@@ -0,0 +1,13 @@
+{
+  "type": "service_account",
+  "project_id": "zeaipc",
+  "private_key_id": "78172a99eb5a7de4a38500fb1329305d65bc6bba",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDHSHWMjLv6ZTEy\nNBvCfSBXfL9ZPnvWqLByqUDRCQ4mP4EKz4lbkgCbwzcf2VokF/ChcnvVEWtNY/F7\nxeb9cFmX6CJ+0taZzMj2I60ES5miOXY+5a/pYwabqO7gZVWzXIVSnWYYVjPdJbqY\ngUyZ3M79Zk1G+3v/m86fKmZZ5IFDWBM+WwbEkDTAI3KcPwo1ca1beKp9fsES7za3\nu1jhTsXW0u9kQ3ZFEE1FgeoNQ8q4pKtp/rLaOOjUDaWCkK+hqTlOy1UCgPXVe6PD\nOEM5NvXdjqAAXNf910yNxOFZ/fCaTCmc9WnOHPV3JxoDCOJkZ53fGsDxTMUKBAfs\n0yh0MXmHAgMBAAECggEAFc6QimpGEa7sFL3OAdjWZqxwdfAX9hPshweNX7J5eWb+\nlHPEaKBP77uLdzE+I2/5NXmFInou/z9+B9jOn5IhQ7WzvKQOtrLJlOGvh6ZMMAvQ\nrC5PMx9SdEsmpKiCGcf4kONHbkdUkpET26defgHDJsiAkVForr5afGFT5OdT284A\nUoATNHvzPO1YuKu+ifx7GEd106st5ddvAQfrhrK9iGlrT86m6j3sNzk9lWvN3h5A\nMuQq6+IgRW7GN4RnBNC1MxmL4N1UT7+nyS/b3j/BqzGU5KPLecIoPocRUzTjN5dN\nXKB2SWKV3QTYj9h6Z3WHBadfXhvDfTMQjyaZvGaocQKBgQD5todRGOL3z5ysEK1M\nbAK/0D3N11/wdAQ0iiweYxCVbaEXrtbS+bZ3/vwgibj/gUMDgeRl83dK4jBjWD11\n4iTQ9n/6ObzYhBKbU0q7lmC27YOpP6eR82igxidOB7pKme3W3OkIc8pMbsthk0aC\nDQnCg4F7yKfUeFadcb99LxJgNwKBgQDMTOUoeDbS2lZ9gkDvHJajRGxaELFv+IXr\np45jOBI872GB/hGaWycOS8WH4SAfDZymxvHzqE2PnEL4p7S/g6nRPzn0BCPvuveR\nu6K+MeM5fuSIei6oGR3oIvPgvO/Mm70Cxh+EUscPEYHOXY1t0a8xXOfI3O00W9On\n6EkhDT3pMQKBgF5oJKaDWyPHSPtAXtRarXtpNkSBHZNhuUlHx8BBQb7MsK+seSlQ\nIS0iqJ2AlAF9FCHaVMg6kKot849LiAjuU27xEpc+hufwnrw8dC+RfcUK7NDq8OZb\n+P13jNqST1ru8R1fbXa1mBDnflrIfEqktUDlyOlMhi7sxApIO9YPGspnAoGAZ8HV\n0EvdbBC/RFbWq5mVV630aXl45CkPurMKOZHMkW3x05c5UW9h0nzhATVwntTG7lyz\nnM+pNHpj+O0TewBfSooV4LwJlEMcYMMuz/F+OUEcx8Nl1rGK4s59lXHfR8cAo1wD\nvaxctGZrymbw4QlRNHy+quCpB6JrSCdMnKChBMECgYBwSm4RX9E4WO6WE/64u10e\nCoWTVTidwLu5ExZZi1Mqd8JsphHoX3SYhu2G7oXKUJ4PWWWDMjUGa87lqwx202Db\nDpIHWhFXPjoyAC48Q8doJgWj5icA7a6H7Pd6cvh9+hmCud6uHwW7urMSnEuQiAt5\nizSQsa3hRueqYQ8bNh0MKA==\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-1990b@zeaipc.iam.gserviceaccount.com",
+  "client_id": "104527757881054329175",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-1990b%40zeaipc.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
