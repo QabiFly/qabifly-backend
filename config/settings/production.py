fcf099afe784fc72b1d341e0b0acf20a8d0291f6
QabiFly <qabifly@zohomail.in>
2026-06-02 10:21:45 +0530

    Add SECURE_PROXY_SSL_HEADER for SSL configuration

--- config/settings/production.py
+++ config/settings/production.py
@@ -27,6 +27,7 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_SSL_REDIRECT            = True
 SESSION_COOKIE_SECURE          = True
 CSRF_COOKIE_SECURE             = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # ─── Storage — Cloudinary ─────────────────────────────────────────────────────
 
